d1bb0601fe7b36c7dd2c6b0b5a3ccdb279efa262
Petteri Kivimäki <dinky_jackson@hotmail.com>
2016-12-23 08:34:46 +0200

    Some test code added.

---
+++ src/src/main/java/com/pkrete/test/project/Main.java
@@ -0,0 +1,13 @@
+package com.pkrete.test.project;
+
+/**
+ * Main class.
+ *
+ * @author Petteri Kivimäki
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test project.");
+    }
+}
